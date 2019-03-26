e0b6b80b2b4ad2a8038538f3816389b33ede611c
Natalia Zartha Suarez <natalia.ti@onoff.com.co>
2019-03-25 22:37:34 -0500

    Crear funcion para envio de correo electronico de forma asyncrona. para futuras aplicaciones

---
+++ MasGlobalTest.UI/ApiControllers/EnvioCorreosController.cs
@@ -0,0 +1,76 @@
+﻿using MasGlobalTest.UI.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MasGlobalTest.UI.ApiControllers
+{
+
+    public class EnvioCorreosController : ApiController
+    {
+        private readonly EmailStructure _emailStructure;
+
+        private EnvioCorreosController()
+        {
+            if (_emailStructure == null)
+                _emailStructure = new EmailStructure();
+
+        }
+
+        //GET: api/EnvioCorreos/SendEmailNodo?subject=asuntoCorreoprueba2&body=este es el cuerpo del correo&emailTo=natzasu13@gmail.com&emailsCopy=nzartha@utp.edu.co&emailsCopy=natalia.zartha@gmail.com&emailsHidderCopy=natzasu13@gmail.com&emailsHidderCopy=natzasu13@hotmail.com
+        [HttpGet]
+        public async Task SendEmailNodo(string subject, string emailTo, string body, [FromUri] string[] emailsCopy, [FromUri] string[] emailsHidderCopy)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpClient = new SmtpClient();
+
+            var tuple = _emailStructure.ConfigEmailStructure(subject, emailTo, body, emailsCopy, emailsHidderCopy);
+            smtpClient = tuple.Item2;
+            mail = tuple.Item1;
+
+            #region add sttachments
+            //Add image
+            string imagePath = AppDomain.CurrentDomain.BaseDirectory + "/Images/bullet.png";
+            Attachment inlineLogo = new Attachment(imagePath);
+            mail.Attachments.Add(inlineLogo);
+            //if (!string.IsNullOrWhiteSpace(attachmentPath) && File.Exists(attachmentPath))
+            //{
+            //    Attachment attachment = new Attachment(attachmentPath);
+            //    mailMsg.Attachments.Add(attachment);
+            //}
+
+            ////Add .doc
+
+            #endregion
+
+
+            //event handler for asynchronous call
+            try
+            {
+                await smtpClient.SendMailAsync(mail);
+            }
+            catch (Exception ex)
+            {
+                /* exception handling code here */
+            }
+
+        }
+
+        public class EmailApiModel
+        {
+            public string subject { get; set; }
+            public string body { get; set; }
+            public string emailTo { get; set; }
+            public string emailFrom { get; set; }
+            public string idNodo { get; set; }
+            public string[] emailsCopy { get; set; }
+            public string[] emailsHidderCopy { get; set; }
+        }
+    }
+}
---
+++ MasGlobalTest.UI/Utilities/EmailStructure.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace MasGlobalTest.UI.Utilities
+{
+    public class EmailStructure
+    {
+        private string passwordFrom = "****";
+        private string emailFrom = "natzasu13@hotmail.com";
+        private int port = 587; //465; //587;
+        private string smtp = "smtp.live.com";
+
+        public EmailStructure()
+        {
+
+
+        }
+
+        public Tuple<MailMessage, SmtpClient> ConfigEmailStructure(string subject, string emailTo, string body, string[] emailsCopy = null, string[] emailsHidderCopy = null)
+        {
+            MailMessage mail = new MailMessage(emailFrom, emailTo);
+
+            #region Email body
+            mail.IsBodyHtml = true;
+            mail.Subject = subject;
+            mail.SubjectEncoding = Encoding.UTF8;
+            mail.Body = body;
+            mail.BodyEncoding = Encoding.UTF8;
+            mail.Priority = MailPriority.Normal;
+
+            #endregion
+
+            #region Add email whit copy and hidder copy
+            if (emailsCopy != null)
+            {
+                foreach (var item in emailsCopy)
+                {
+                    MailAddress copy = new MailAddress(item);
+                    mail.CC.Add(copy);
+                }
+            }
+
+            if (emailsHidderCopy != null)
+            {
+                foreach (var item in emailsHidderCopy)
+                {
+                    MailAddress Bcopy = new MailAddress(item);
+                    mail.Bcc.Add(Bcopy);
+                }
+            }
+            #endregion
+
+            #region Smtp cliente congiguration
+
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Port = port;
+            smtpClient.Host = smtp;
+            smtpClient.Timeout = 1000000;
+            smtpClient.UseDefaultCredentials = false; // Important: This line of code must be executed before setting the NetworkCredentials object, otherwise the setting will be reset (a bug in .NET)
+            smtpClient.Credentials = new System.Net.NetworkCredential(emailFrom, passwordFrom);
+            //detect SSL/TLS automatically
+            smtpClient.EnableSsl = true; //Esto es para que vaya a través de SSL que es obligatorio con GMail
+
+            #endregion
+
+            return Tuple.Create(mail, smtpClient);
+        }
+    }
+}
